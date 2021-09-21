6f1ffa55d4e83b001edd71445e376407787adc24
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-21 17:59:10 +0700

    Arg check on MbStrrposEncodingArgumentPositionRector

--- rules/Php74/Rector/FuncCall/MbStrrposEncodingArgumentPositionRector.php
+++ rules/Php74/Rector/FuncCall/MbStrrposEncodingArgumentPositionRector.php
@@ -53,6 +53,10 @@ final class MbStrrposEncodingArgumentPositionRector extends AbstractRector
             return null;
         }
 
+        if (! $node->args[2] instanceof Arg) {
+            return null;
+        }
+
         $secondArgType = $this->getStaticType($node->args[2]->value);
         if ($secondArgType instanceof IntegerType) {
             return null;
